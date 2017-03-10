5566662c745144ea80a0ebc684da0eeee18c332e
khai96_ <hvksmr1996@gmail.com>
2017-03-10 22:44:56 +0700

    Add cv namespace

--- 1/load-an-image.cpp
+++ 1/load-an-image.cpp
@@ -1,7 +1,7 @@
 #include <opencv/highgui.h>
 
 int main () {
-  Mat image = cv::imread("image/alpha");
+  cv::Mat image = cv::imread("image/alpha");
   cv::namedWindow("window");
   cv::imshow("window", image);
   cv::waitKey();
