b0920284dd88359d0295bf4774ce268eeb3e6c86
khai96_ <hvksmr1996@gmail.com>
2017-03-11 12:52:53 +0700

    Use the right framerate

--- 1/load-a-video.cpp
+++ 1/load-a-video.cpp
@@ -7,11 +7,12 @@ using namespace cv;
 int main () {
   namedWindow("window", CV_WINDOW_NORMAL);
   VideoCapture video("video/.mp4");
+  const int delay = 1000.0f / video.get(CV_CAP_PROP_FPS);
   do {
     Mat frame;
     video >> frame;
     if (frame.empty()) break;
     imshow("window", frame);
-  } while (waitKey(30) < 0);
+  } while (waitKey(delay) < 0);
   return EXIT_SUCCESS;
 }
