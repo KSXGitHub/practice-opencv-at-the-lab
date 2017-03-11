bb47bcc3aa376cc9c49507d09751ff213df85937
khai96_ <hvksmr1996@gmail.com>
2017-03-11 12:50:41 +0700

    Change structure

--- 1/load-a-video.cpp
+++ 1/load-a-video.cpp
@@ -7,12 +7,11 @@ using namespace cv;
 int main () {
   namedWindow("window", CV_WINDOW_NORMAL);
   VideoCapture video("video/.mp4");
-  for ( ; ; ) {
+  do {
     Mat frame;
     video >> frame;
     if (frame.empty()) break;
     imshow("window", frame);
-    if (waitKey(30) >= 0) break;
-  }
+  } while (waitKey(30) < 0);
   return EXIT_SUCCESS;
 }
